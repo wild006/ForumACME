c06bff344fe7cb5ee6a5c9e28ca273a13b2da48c
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-12-07 18:46:24 -0500

    Modifications des vues.
       * SujetVue: La classe pour afficher tout les sujets de la bd
       * MessageVue: La classe pour afficher les messages en fonction du sujet

--- forum.py
+++ forum.py
@@ -29,28 +29,40 @@ class Sujet():
         self.dernier = dernier
         self.parent = parent
 
-class Vue():
+class SujetVue():
     def __init__(self, commandes):
         self.forum = Tk()
         self.commandes = commandes
         
-        self.mlb = MultiListbox(self.forum, (('Message', 40), ('Date', 20), ('Nombre de messages', 10)))
+        self.boxSujet = MultiListbox(self.forum, (('Message', 40), ('Date', 20), ('Nombre de messages', 10)))
         self.remplirListe()
-        #for i in range(1000):
-        #    mlb.insert(END, ('Important Message: %d' % i, 'John Doe', '10/10/%04d' % (1900+i)))
-        self.mlb.pack(expand=YES,fill=BOTH)
+        self.boxSujet.pack(expand=YES,fill=BOTH)
 
-        btn = Button(self.forum, text="stuff", command=lambda: self.visioner(self.mlb.curselection()))
+        btn = Button(self.forum, text="stuff", command=lambda: self.visioner(self.boxSujet.curselection()))
         btn.pack()
 
     def remplirListe(self):
-        sujets = self.commandes.searchSujet()
-        print("sujets", sujets)
+        sujets = self.commandes.searchSujet() #[Sujet(0, "Un", "hier", "2", "demain", None), Sujet(1, "Deux", "demain", "3", "hier", None)]
         for sujet in sujets:
-            self.mlb.insert(END, ('Important Message: ' + sujet.nom, sujet.date, sujet.nbMessages))
+            self.boxSujet.insert(END, ('Important Message: ' + sujet.nom, sujet.date, sujet.nbMessages))
 
-    def visioner(self,event):
-       print (event)
+    def visioner(self, event):
+        MessageVue(event[0], self.commandes)
 
-    def supprimer(self,event):
+    def supprimer(self, event):
         pass
+
+class MessageVue():
+    def __init__(self, n, commandes):
+        """Affiche les messages du sujet a l'id `n'"""
+        self.commandes = commandes
+        self.id = n
+        self.mess = Tk()
+        self.message = MultiListbox(self.mess, (('Texte', 40), ('Auteur', 20), ('Date', 10)))
+        self.message.pack(expand=YES, fill=BOTH)
+        self.remplirListe()
+
+    def remplirListe(self):
+        messages = self.commandes.trouveSujet(self.id) #[Message(0, "Premier", "Jamais", "Moi", self.id), Message(1, "Second", "Toujours", "L'autre", self.id)]
+        for m in messages:
+            self.message.insert(END,(m.texte, m.auteur, m.date))
