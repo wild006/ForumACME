df0b9118eb499adf75c5d78218d115191df91b37
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-12-13 15:00:47 -0500

    Ajout de messages et sujets

--- forum.py
+++ forum.py
@@ -41,9 +41,11 @@ class SujetVue():
 
         Button(self.forum, text="Visioner", command=lambda: self.visioner(self.boxSujet.curselection())).pack()
         Button(self.forum, text="Supprimer", command=lambda: self.supprimer(self.boxSujet.curselection())).pack()
+        Button(self.forum, text="Ajouter", command=self.ajouter).pack()
 
     def remplirListe(self):
-        sujets = self.commandes.searchSujets() #[Sujet(0, "Un", "hier", "2", "demain", None), Sujet(1, "Deux", "demain", "3", "hier", None)]
+        self.boxSujet.delete(0, END)
+        sujets = self.commandes.searchSujets() #[Sujet(1, "Un", "hier", "2", "demain", None), Sujet(2, "Deux", "demain", "3", "hier", None)]
         for sujet in sujets:
             self.boxSujet.insert(END, ('Important Message: ' + sujet.nom, sujet.date, sujet.nbMessages))
 
@@ -53,6 +55,21 @@ class SujetVue():
     def supprimer(self, event):
         self.commandes.supprimerSujetParID(event[0]) # TODO: Le faire :D
 
+    def ajouter(self):
+        nsujet = Tk()
+        Label(nsujet, text="Titre").grid(column = 0, row = 0)
+        titre = Entry(nsujet)
+        titre.grid(column = 1, row = 0)
+        Label(nsujet, text="Sujet").grid(column = 0, row = 1)
+        sujet = Entry(nsujet)
+        sujet.grid(column = 1, row = 1)
+        Button(nsujet, text="Go", command=lambda:self.nouveau_sujet(nsujet, titre.get(), sujet.get())).grid()
+
+    def nouveau_sujet(self, nsujet, titre, sujet):
+        self.commandes.ajouteSujet(titre, sujet)
+        nsujet.destroy()
+        self.remplirListe()
+        
 class MessageVue():
     def __init__(self, n, commandes):
         """Affiche les messages du sujet a l'id `n'"""
@@ -67,12 +84,21 @@ class MessageVue():
         Button(self.mess, text="Supprimer", command=lambda: self.supprimer(self.message.curselection())).pack()
 
     def remplirListe(self):
-        messages = self.commandes.searchMessages(self.id) #[Message(0, "Premier", "Jamais", "Moi", self.id), Message(1, "Second", "Toujours", "L'autre", self.id)]
+        self.message.delete(0, END)
+        messages = self.commandes.searchMessages(self.id) #[Message(1, "Premier", "Jamais", "Moi", self.id, self.id), Message(2, "Second", "Toujours", "L'autre", self.id, self.id)]
         for m in messages:
             self.message.insert(END,(m.texte, m.auteur, m.date))
 
     def ajouter(self):
-        Text().pack()
+        nmess = Tk()
+        texte = Text(nmess)
+        texte.pack()
+        Button(nmess, text="Envoyer", command=lambda: self.nouveau_message(nmess, texte)).pack()
+
+    def nouveau_message(self, nmess, texte):
+        self.commandes.ajouteMessage(texte.get("1.0", END), self.id) # END ajoute un \n à la fin. On veut ça?
+        nmess.destroy()
+        self.remplirListe()
 
     def supprimer(self, n):
         self.commandes.supprimerMessageParID(self.id, n) # TODO: Prend l'id du sujet pour trouver le bon message
